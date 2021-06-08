ff3b9614c720824174fd1947a6f5e0140637e5d4
Avinash Kumar <itsmeatg@outlook.com>
2021-06-08 12:48:59 +0530

    Homepage Layout Updation Required

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -8,10 +8,11 @@ import SubList from "../components/sub/SubList";
 const Home = () => {
   return (
     <>
-      <div className="jumbotron text-danger h1 font-weight-bold text-center">
-        <Jumbotron text={["Latest Products", "New Arrivals", "Best Sellers"]} />
+      <div className="breadcrumb text-danger h1 font-weight-bold text-center">
+        <Jumbotron text={["More Design on Dresses",
+                          "Things related to your Intrested",
+                          "Compartmental Accessories"]} />
       </div>
-
       <h4 className="text-center p-3 mt-5 mb-5 display-4 jumbotron">
         New Arrivals
       </h4>
